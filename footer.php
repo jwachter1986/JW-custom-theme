c6c14e9ecc6d3593eae62f16e08ff0db34fba1bc
jwachter1986 <39983270+jwachter1986@users.noreply.github.com>
2018-06-25 06:21:13 -0700

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-        © 2018 First Class Web Development
-    </footer>
-
-    <?php wp_footer(); ?>
-    </body>
-</html>
